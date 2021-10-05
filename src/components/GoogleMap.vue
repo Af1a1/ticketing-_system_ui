79ce202dde8710275ff863dee50ad390e0e2ab63
Af1a1 <aflalahamed0124@gmail.com>
2021-10-05 09:25:15 +0530

    Map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -10,12 +10,29 @@
 </template>
 
 <script>
+import RestAdapter from '@/restAdapter/index'
 export default {
   name: 'GoogleMap',
   data() {
     return {
       center: { lat: 45.508, lng: -73.587 },
+      mapDetails: ''
     }
   },
+  methods:{
+    async getMap(id){
+      try{
+        const Response = await RestAdapter.get(`/api/v1/journey/${id}`)
+        const mapDetails = Response.data.data.map((x)=>({
+          latitude: x.latitude,
+          longitude: x.logtitude,
+        }))
+        this.center.lat = mapDetails.latitude
+        this.center.lng = mapDetails.longitude
+      }catch(error){
+        console.error();
+      }
+    }
+  }
 };
 </script>
